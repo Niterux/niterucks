dbe8d87725d8791177ecb14a8982cff770499422
Niterux <parkerdec@gmail.com>
2023-07-06 12:29:28 -0400

    we have life

--- src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
@@ -1,19 +1,17 @@
 package com.example.example_mod.mixin;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.example.example_mod.ExampleMod;
-
-import net.minecraft.client.gui.screen.TitleScreen;
-
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class TitleScreenMixin extends Screen {
 
-	@Inject(method = "init", at = @At("TAIL"))
+	@Inject(method = "render", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		this.drawString(this.textRenderer, "sex", 2, 10, 5263440);
 	}
 }
