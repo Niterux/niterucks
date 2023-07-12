41bf414ff9447365a32e5642c794bf04d14192a3
Niterux <parkerdec@gmail.com>
2023-07-12 18:40:31 -0400

    lol

---
+++ src/main/java/com/niterux/niterucks/mixin/ForcePiracy.java
@@ -0,0 +1,10 @@
+package com.niterux.niterucks.mixin;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Minecraft.class)
+public class ForcePiracy {
+	@Shadow
+	public static long f_4942690 = 1L;
+}
