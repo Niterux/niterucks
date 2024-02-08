e3fa6ec02fabe49a12a6cd33f92d8441f4cca0be
moehreag <moehreag@gmail.com>
2024-02-08 18:32:45 +0100

    clean up a few things

--- src/main/java/io/github/niterux/niterucks/config/widget/NiterucksCategoryListWidget.java
+++ src/main/java/io/github/niterux/niterucks/config/widget/NiterucksCategoryListWidget.java
@@ -5,20 +5,23 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import io.github.axolotlclient.AxolotlClientConfig.api.manager.ConfigManager;
-import io.github.axolotlclient.AxolotlClientConfig.api.options.Option;
 import io.github.axolotlclient.AxolotlClientConfig.api.options.OptionCategory;
 import io.github.axolotlclient.AxolotlClientConfig.api.options.WidgetIdentifieable;
 import io.github.axolotlclient.AxolotlClientConfig.impl.ui.ClickableWidget;
 import io.github.axolotlclient.AxolotlClientConfig.impl.ui.vanilla.widgets.VanillaButtonListWidget;
 import io.github.axolotlclient.AxolotlClientConfig.impl.util.ConfigStyles;
+import io.github.axolotlclient.AxolotlClientConfig.impl.util.DrawUtil;
 import io.github.axolotlclient.AxolotlClientConfig.impl.util.MathUtil;
+import net.minecraft.resource.Identifier;
 import net.minecraft.resource.language.I18n;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.opengl.GL11;
 
 public class NiterucksCategoryListWidget extends VanillaButtonListWidget {
+	private static final Identifier BACKGROUND_TEXTURE = new Identifier("/gui/background.png");
 	private OptionCategory current;
 	private static double scroll;
 	public NiterucksCategoryListWidget(ConfigManager manager, OptionCategory category, int screenWidth, int screenHeight, int top, int bottom, int entryHeight) {
@@ -31,11 +34,6 @@ public class NiterucksCategoryListWidget extends VanillaButtonListWidget {
 	public void addEntry(OptionCategory first, @Nullable OptionCategory second) {
 		addEntry(createCategoryEntry(createWidget(10, first), first,
 			second == null ? null : createWidget(width / 2 + WIDGET_ROW_RIGHT, second), second));
-	}
-
-	@Override
-	protected void addOptions(ConfigManager manager, Collection<Option<?>> options) {
-
 	}
 
 	@Override
@@ -69,19 +67,19 @@ public class NiterucksCategoryListWidget extends VanillaButtonListWidget {
 	@Override
 	protected void renderBackground() {
 		if (client.world == null) {
-			GL11.glDisable(2896);
-			GL11.glDisable(2912);
-			BufferBuilder var16 = BufferBuilder.INSTANCE;
-			GL11.glBindTexture(3553, client.textureManager.load("/gui/background.png"));
+			GlStateManager.disableLighting();
+			GL11.glDisable(GL11.GL_FOG);
+			BufferBuilder builder = BufferBuilder.INSTANCE;
+			DrawUtil.bindTexture(BACKGROUND_TEXTURE);
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			float var17 = 32.0F;
-			var16.start();
-			var16.color(2105376);
-			var16.vertex(left, bottom, 0.0, (left / var17), ((float) (bottom + (int) this.getScrollAmount()) / var17));
-			var16.vertex(this.right, this.bottom, 0.0, ((float) this.right / var17), ((float) (this.bottom + (int) this.getScrollAmount()) / var17));
-			var16.vertex(this.right, this.top, 0.0, ((float) this.right / var17), ((float) (this.top + (int) this.getScrollAmount()) / var17));
-			var16.vertex(this.left, this.top, 0.0, ((float) this.left / var17), ((float) (this.top + (int) this.getScrollAmount()) / var17));
-			var16.end();
+			float textureWidth = 32.0F;
+			builder.start();
+			builder.color(0x202020);
+			builder.vertex(this.left, this.bottom, 0.0, (this.left / textureWidth), ((float) (this.bottom + (int) this.getScrollAmount()) / textureWidth));
+			builder.vertex(this.right, this.bottom, 0.0, ((float) this.right / textureWidth), ((float) (this.bottom + (int) this.getScrollAmount()) / textureWidth));
+			builder.vertex(this.right, this.top, 0.0, ((float) this.right / textureWidth), ((float) (this.top + (int) this.getScrollAmount()) / textureWidth));
+			builder.vertex(this.left, this.top, 0.0, ((float) this.left / textureWidth), ((float) (this.top + (int) this.getScrollAmount()) / textureWidth));
+			builder.end();
 		}
 	}
 
