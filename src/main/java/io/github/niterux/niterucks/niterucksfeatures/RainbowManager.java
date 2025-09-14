5d88a04362ddc694eb836d61bd9eb9b9415db2a1
Niterux <parkerdec@gmail.com>
2025-09-14 15:46:53 -0400

    optimize RainbowManager

--- src/main/java/io/github/niterux/niterucks/niterucksfeatures/RainbowManager.java
+++ src/main/java/io/github/niterux/niterucks/niterucksfeatures/RainbowManager.java
@@ -1,13 +1,11 @@
 package io.github.niterux.niterucks.niterucksfeatures;
 
-import dev.kdrag0n.colorkt.conversion.ConversionGraph;
 import dev.kdrag0n.colorkt.rgb.Srgb;
 import dev.kdrag0n.colorkt.ucs.lch.Oklch;
 import io.github.niterux.niterucks.Niterucks;
 
 public class RainbowManager {
 	private static final double[] RGB = new double[3];
-	private static Oklch currColor = new Oklch(0.75, 0.12, 160);
 
 	public static int getColor() {
 		int red = (int) (Math.round(RGB[0] * 255) << 16);
@@ -21,16 +19,13 @@ public class RainbowManager {
 	}
 
 	public static void computeRainbow() {
-		currColor = new Oklch(currColor.getLightness(), currColor.getChroma(), getRotation());
-		Srgb SRGBColor = (Srgb) ConversionGraph.convert(currColor, kotlin.jvm.JvmClassMappingKt.getKotlinClass(Srgb.class));
-
-		assert SRGBColor != null;
+		Srgb SRGBColor = new Oklch(0.75, 0.12, getRotation()).toOklab().toLinearSrgb().toSrgb();
 		RGB[0] = SRGBColor.getR();
 		RGB[1] = SRGBColor.getG();
 		RGB[2] = SRGBColor.getB();
 	}
 
 	private static double getRotation() {
-		return ((System.currentTimeMillis() * 360 * Niterucks.CONFIG.rainbowSpeed.get()) / 1000) % 360;
+		return ((System.currentTimeMillis() * Niterucks.CONFIG.rainbowSpeed.get()) / (1000d / 360d)) % 360d;
 	}
 }
