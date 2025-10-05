e2ad3e9c8212c999cec982bf4aa5024d751a1628
Niterux <parkerdec@gmail.com>
2025-10-05 19:42:31 -0400

    Increase the packet limit to 10k

--- src/main/java/io/github/niterux/niterucks/mixin/ConnectionMixin.java
+++ src/main/java/io/github/niterux/niterucks/mixin/ConnectionMixin.java
@@ -1,5 +1,6 @@
 package io.github.niterux.niterucks.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,5 +16,10 @@ public class ConnectionMixin {
 	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/net/Socket;setTrafficClass(I)V"))
 	private void reducePing(Socket socket, String address, PacketHandler listener, CallbackInfo ci) throws SocketException {
 		socket.setTcpNoDelay(true);
+	}
+
+	@ModifyExpressionValue(method = "tick()V", at = @At(value = "CONSTANT", args = "intValue=100"))
+	private int increasePacketLimit(int original) {
+		return Math.max(original, 10000);
 	}
 }
